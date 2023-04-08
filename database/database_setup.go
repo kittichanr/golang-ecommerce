c1e40bdf9203217ac5d446711bc6d87358ee8979
petchkubb <petchkubbb@gmail.com>
2023-04-08 16:31:34 +0700

    implememt sigup login fuc

--- database/database_setup.go
+++ database/database_setup.go
@@ -38,9 +38,11 @@ func DBSet() *mongo.Client {
 var Client *mongo.Client = DBSet()
 
 func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
-
+	var collection *mongo.Collection = (*mongo.Collection)(client.Database("Ecommerce").Collection(collectionName))
+	return collection
 }
 
 func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
-
+	var productCollection *mongo.Collection = (*mongo.Collection)(client.Database("Ecommerce").Collection(collectionName))
+	return productCollection
 }
