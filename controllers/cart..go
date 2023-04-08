273fea16f1936bd925a7385e1ac828b43bd41e44
petchkubb <petchkubbb@gmail.com>
2023-04-08 14:13:59 +0700

    create empty func controllers

---
+++ controllers/cart..go
@@ -0,0 +1,19 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func AddToCart() gin.HandlerFunc {
+
+}
+
+func RemoteItem() gin.HandlerFunc {
+
+}
+
+func GetItemFromCart() gin.HandlerFunc {
+
+}
+
+func BuyFromCart() gin.HandlerFunc {
+
+}
---
+++ controllers/controllers.go
@@ -0,0 +1,31 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPassword(userPassword string, givenPassword string) bool {
+
+}
+
+func Signup() gin.HandlerFunc {
+
+}
+
+func Login() gin.HandlerFunc {
+
+}
+
+func ProductViewerAdmin() gin.HandlerFunc {
+
+}
+
+func searchProduct() gin.HandlerFunc {
+
+}
+
+func searchProductByQuery() gin.HandlerFunc {
+
+}
