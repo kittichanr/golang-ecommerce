bf718d46ecb1d82cb16a315ae8b040f383949dc3
petchkubb <petchkubbb@gmail.com>
2023-04-09 11:24:20 +0700

    setup docker

--- main.go
+++ main.go
@@ -16,19 +16,20 @@ func main() {
 	if port == "" {
 		port = "8000"
 	}
-
 	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
 
 	router := gin.New()
 	router.Use(gin.Logger())
-
 	routes.UserRoutes(router)
 	router.Use(middleware.Authentication())
-
 	router.GET("/addtocart", app.AddToCart())
 	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/listcart", app.GetItemFromCart())
+	router.POST("/addaddress", controllers.AddAddress())
+	router.PUT("/edithomeaddress", controllers.EditHomeAddress())
+	router.PUT("/editworkaddress", controllers.EditWorkAddress())
+	router.GET("/deleteaddresses", controllers.DeleteAddress())
 	router.GET("/cartcheckout", app.BuyFromCart())
 	router.GET("/instantbuy", app.InstantBuy())
-
 	log.Fatal(router.Run(":" + port))
 }
