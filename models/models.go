87c92a4b2d3492a0022e4cf4df5386ca41cc1d82
petchkubb <petchkubbb@gmail.com>
2023-04-09 10:52:45 +0700

    implement more db func

--- models/models.go
+++ models/models.go
@@ -50,7 +50,7 @@ type Address struct {
 type Order struct {
 	Order_ID       primitive.ObjectID `bson:"_id"`
 	Order_Cart     []ProductUser      `json:"order_list" bson:"order_list"`
-	Ordered_At     time.Time          `json:"ordered_at" bson:"ordered_at"`
+	Orderered_At   time.Time          `json:"ordered_on" bson:"ordered_at"`
 	Price          int                `json:"total_price" bson:"total_price"`
 	Discount       *int               `json:"discount" bson:"discount"`
 	Payment_Method Payment            `json:"payment_method" bson:"payment_method"`
